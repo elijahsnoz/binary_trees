2a27b9ac92ac60b45055fa704cff166b851c91bd
azzaomer <azzaomer70@gmail.com>
2024-06-07 13:59:25 +0300

    tree height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t left_height = 0;
+    size_t right_height = 0;
+
+    if (tree == NULL)
+    {
+        return 0;
+    }
+
+    left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+    right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+    // Return the greater height of the two subtrees plus one for the current node
+    return (left_height > right_height ? left_height : right_height);
+
+}
--- binary_trees.h
+++ binary_trees.h
@@ -50,6 +50,9 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 /*function that counts the nodes with at least 1 child in a binary tree*/
 size_t binary_tree_nodes(const binary_tree_t *tree);
 
+/* function that measures the height of a binary tree*/
+size_t binary_tree_height(const binary_tree_t *tree);
+
 /*function that measures the balance factor of a binary tree*/
 int binary_tree_balance(const binary_tree_t *tree);
 
