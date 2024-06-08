12e3eae4256bc9264dc723a8fad042f0b62ae077
azzaomer <azzaomer70@gmail.com>
2024-06-08 13:01:20 -0700

    rotate ledft

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -6,24 +6,22 @@
  *
  * Return: A pointer to the new root node of the tree once rotated
  */
+
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-    binary_tree_t *new_root, *temp;
+	binary_tree_t *new_root, *temp;
 
-    if (tree == NULL || tree->right == NULL)
-        return (NULL);
+	if (tree == NULL || tree->right == NULL)
+		return (NULL);
+	new_root = tree->right;
+	temp = new_root->left;
 
-    new_root = tree->right;
-    temp = new_root->left;
+	new_root->left = tree;
+	tree->parent = new_root;
 
-    new_root->left = tree;
-    tree->parent = new_root;
-
-    tree->right = temp;
-    if (temp != NULL)
-        temp->parent = tree;
-
-    new_root->parent = NULL;
-
-    return (new_root);
+	tree->right = temp;
+	if (temp != NULL)
+		temp->parent = tree;
+	new_root->parent = NULL;
+	return (new_root);
 }
